98b55253af1410222295d8db0c8d3054656e0508
Nicholai Gallegos <gallegon@oregonstate.edu>
2021-03-11 09:10:08 -0800

    fixed linting errors on test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,13 +1,10 @@
-import pytest
 import calculator
 
 
 class TestCalculator:
-	
 
-	def test_add(self):
-		assert calculator.add(4,5) == 9
+    def test_add(self):
+        assert calculator.add(4, 5) == 9
 
-	
-	def test_subtract(self):
-		assert calculator.subtract(5,3) == 2
+    def test_subtract(self):
+        assert calculator.subtract(5, 3) == 2
